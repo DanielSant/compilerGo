10f10ed2873eb92cc9976d4f208d9adb59bf6ed7
WagnerLinux <vagneraprata@gmail.com>
2020-03-17 11:32:41 -0300

    Atualizaçao GoParser

--- Compilador/GoParser.py
+++ Compilador/GoParser.py
@@ -1,36 +1,44 @@
 def m_functionDecl(p):
     '''functionDecl : FUNC ID signature
-                    | FUNC ID signature functionBody'''
+                    | FUNC ID signature functionBody '''
 
 def m_signature(p):
     '''signature : parameters
-                 | parameters result'''
+                 | parameters result '''
 
 def m_result(p):
-    '''result : type'''
+    '''result : type '''
 
 def m_type(p):
     '''type : INT
             | STRING
             | BOOL
             | BYTE
-            | FLOAT'''
+            | FLOAT '''
 
 def m_parameters(p):
-    '''parameters : LPAREN parameterList COMMA RPAREN''' 
+    '''parameters : LPAREN parameterList COMMA RPAREN ''' 
 
 def m_parameterList(p):
-    '''parameterList : parameterDecl COMMA parameterDecl'''
+    '''parameterList : parameterDecl COMMA parameterDecl '''
 
 def m_parameterDecl(p):
-    '''parameterDecl : identifier TYPE'''
+    '''parameterDecl : identifier TYPE '''
 
 def m_functionBody(p):
     '''functionBody : block''' ### OBSERVAÇÃO
 
 def m_block(p):
-    '''block : LCHAVES statementList RCHAVES'''
+    '''block : LCHAVES statementList RCHAVES '''
 
 def m_statementList(p):
-    '''
+    '''statementList : declaration
+                     | simpleStmt
+                     | returnStmt
+                     | breakStmt
+                     | continueStmt
+                     | block  
+                     | ifStmt
+                     | switchStmt
+                     | forStmt '''
 
